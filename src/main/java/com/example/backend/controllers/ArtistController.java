b2c2a42787bd160591dbc7cd504c217459f6bf21
unknown <logos.vera@mail.ru>
2023-04-27 16:50:36 +0300

    lab 5-6

---
+++ src/main/java/com/example/backend/controllers/ArtistController.java
@@ -0,0 +1,74 @@
+package com.example.backend.controllers;
+
+import com.example.backend.models.Artist;
+import com.example.backend.repositories.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.backend.models.Country;
+import com.example.backend.repositories.CountryRepository;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ArtistController {
+    @Autowired
+    ArtistRepository artistRepository;
+    CountryRepository countryRepository;
+
+    public ArtistController(ArtistRepository artistRepository, CountryRepository countryRepository) {
+        this.artistRepository = artistRepository;
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping("/artists")
+    public List
+    getAllArtists() {
+        return artistRepository.findAll();
+    }
+
+    @PostMapping("/artists")
+    public ResponseEntity<Object> createArtist(@RequestBody Artist artist) throws Exception {
+        try {
+            Artist nc = artistRepository.save(artist);
+            return new ResponseEntity<Object>(nc, HttpStatus.OK);
+        }
+        catch(Exception ex) {
+            String error;
+            if (ex.getMessage().contains("artists.name_UNIQUE"))
+                error = "artistalreadyexists";
+            else
+                error = "undefinederror";
+            Map<String, String>
+                    map =  new HashMap<>();
+            map.put("error", error);
+            return new ResponseEntity<Object> (map, HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("/artists/{id}")
+    public ResponseEntity<Object> deleteArtist(@PathVariable(value = "id") Long artistId) {
+        Optional<Artist>
+                artist = artistRepository.findById(artistId);
+        Map<String, Boolean>
+                resp = new HashMap<>();
+        if (artist.isPresent()) {
+            artistRepository.delete(artist.get());
+            resp.put("deleted", Boolean.TRUE);
+        }
+        else
+            resp.put("deleted", Boolean.FALSE);
+        return ResponseEntity.ok(resp);
+    }
+
+    @GetMapping("/countries/{id}/artists")
+    public ResponseEntity<List<Artist>> getCountryArtists(@PathVariable(value = "id") Long countryId) {
+        Optional<Country> cc = countryRepository.findById(countryId);
+        if (cc.isPresent()) {
+            return ResponseEntity.ok(cc.get().artists);
+        }
+        return ResponseEntity.ok(new ArrayList<Artist>());
+    }
+}
