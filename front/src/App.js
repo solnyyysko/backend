f32c7d8d8aa5aab0ba89a3a2600f8a2d43ebf63f
unknown <logos.vera@mail.ru>
2023-05-17 10:30:48 +0300

    lab8

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import {createBrowserHistory} from "history";
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+export default App;
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavigationBar />
+                <div className="container-fluid">
+                    <Routes>
+                        <Route path="home" element={<Home />}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
